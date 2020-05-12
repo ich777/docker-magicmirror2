cb59c10403d03748312e07d1459cd34c2b488e6e
ich777 <ich777@noreply.example.org>
2020-05-12 16:53:54 +0200

    „scripts/start-server.sh“ ändern

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -6,52 +6,145 @@ if [ -z $LAT_V ]; then
 	sleep infinity
 fi
 
-if [ -z "$CUR_V" ]; then
-	echo "---MagicMirror² not found!---"
-	cd ${DATA_DIR}
-	if wget -q -nc --show-progress --progress=bar:force:noscroll -O $LAT_V.tar.gz https://github.com/MichMich/MagicMirror/archive/$LAT_V.tar.gz ; then
-		echo "---Successfully downloaded MagicMirror² $LAT_V!---"
+if [ "${FORCE_UPDATE}" == "true" ]; then
+	echo "---Force Update enabled!---"
+	if [ -z "$CUR_V" ]; then
+		cd ${DATA_DIR}
+		if wget -q -nc --show-progress --progress=bar:force:noscroll -O $LAT_V.tar.gz https://github.com/MichMich/MagicMirror/archive/$LAT_V.tar.gz ; then
+			echo "---Successfully downloaded MagicMirror² $LAT_V!---"
+		else
+			echo "---Something went wrong, can't download Magic Mirror² $LAT_V, putting server in sleep mode---"
+			sleep infinity
+		fi
+		tar -xvf $LAT_V.tar.gz
+		mv ${DATA_DIR}/MagicMirror*/* ${DATA_DIR}/
+		rm -R ${DATA_DIR}/MagicMirror*
+		rm -R ${DATA_DIR}/$LAT_V.tar.gz
+		touch ${DATA_DIR}/installed$LAT_V
+		if [ ! -f ${DATA_DIR}/modules/modules.txt ]; then
+			cp /tmp/modules.txt ${DATA_DIR}/modules/modules.txt
+		fi
 	else
-		echo "---Something went wrong, can't download Magic Mirror² $LAT_V, putting server in sleep mode---"
-		sleep infinity
-	fi
-	tar -xvf $LAT_V.tar.gz
-	mv ${DATA_DIR}/MagicMirror*/* ${DATA_DIR}/
-	rm -R ${DATA_DIR}/MagicMirror*
-	rm -R ${DATA_DIR}/$LAT_V.tar.gz
-	touch ${DATA_DIR}/installed$LAT_V
-elif [ "$LAT_V" != "$CUR_V" ]; then
-	echo "---Newer version found, installing!---"
-	mkdir -p /tmp/Backup
-	cd /tmp/Backup
-	if wget -q -nc --show-progress --progress=bar:force:noscroll -O $LAT_V.tar.gz https://github.com/MichMich/MagicMirror/archive/$LAT_V.tar.gz ; then
-		echo "---Successfully downloaded MagicMirror² $LAT_V!---"
-	else
-		echo "---Something went wrong, can't download MagicMirror² $LAT_V, putting server in sleep mode---"
-		sleep infinity
-	fi
-	mv ${DATA_DIR}/config /tmp/Backup/
-	mv ${DATA_DIR}/modules /tmp/Backup/
-	rm -R ${DATA_DIR}/*
-	rm -R ${DATA_DIR}/.*
-	mv /tmp/Backup/$LAT_V.tar.gz ${DATA_DIR}
-	cd ${DATA_DIR}
-	tar -xvf $LAT_V.tar.gz
-	mv ${DATA_DIR}/MagicMirror*/* ${DATA_DIR}/
-	rm -R ${DATA_DIR}/MagicMirror*
-	rm -R ${DATA_DIR}/$LAT_V.tar.gz
-	rm -R ${DATA_DIR}/config
-	rm -R ${DATA_DIR}/modules
-	mv /tmp/Backup/config ${DATA_DIR}/
-	mv /tmp/Backup/modules ${DATA_DIR}/
-	rm -R /tmp/Backup
-	touch ${DATA_DIR}/installed$LAT_V
-elif [ "$LAT_V" == "$CUR_V" ]; then
-   echo "---MagicMirror² up-to-date---"
+		mkdir -p /tmp/Backup
+		cd /tmp/Backup
+		if wget -q -nc --show-progress --progress=bar:force:noscroll -O $LAT_V.tar.gz https://github.com/MichMich/MagicMirror/archive/$LAT_V.tar.gz ; then
+			echo "---Successfully downloaded MagicMirror² $LAT_V!---"
+		else
+			echo "---Something went wrong, can't download MagicMirror² $LAT_V, putting server in sleep mode---"
+			sleep infinity
+		fi
+		mv ${DATA_DIR}/config /tmp/Backup/
+		mv ${DATA_DIR}/modules /tmp/Backup/
+		rm -R ${DATA_DIR}/*
+		rm -R ${DATA_DIR}/.*
+		mv /tmp/Backup/$LAT_V.tar.gz ${DATA_DIR}
+		cd ${DATA_DIR}
+		tar -xvf $LAT_V.tar.gz
+		mv ${DATA_DIR}/MagicMirror*/* ${DATA_DIR}/
+		rm -R ${DATA_DIR}/MagicMirror*
+		rm -R ${DATA_DIR}/$LAT_V.tar.gz
+		rm -R ${DATA_DIR}/config
+		rm -R ${DATA_DIR}/modules
+		mv /tmp/Backup/config ${DATA_DIR}/
+		mv /tmp/Backup/modules ${DATA_DIR}/
+		rm -R /tmp/Backup
+		touch ${DATA_DIR}/installed$LAT_V
+		if [ ! -f ${DATA_DIR}/modules/modules.txt ]; then
+			cp /tmp/modules.txt ${DATA_DIR}/modules/modules.txt
+		fi
 else
-   echo "---Something went wrong, putting server in sleep mode---"
-   sleep infinity
+	if [ -z "$CUR_V" ]; then
+		echo "---MagicMirror² not found!---"
+		cd ${DATA_DIR}
+		if wget -q -nc --show-progress --progress=bar:force:noscroll -O $LAT_V.tar.gz https://github.com/MichMich/MagicMirror/archive/$LAT_V.tar.gz ; then
+			echo "---Successfully downloaded MagicMirror² $LAT_V!---"
+		else
+			echo "---Something went wrong, can't download Magic Mirror² $LAT_V, putting server in sleep mode---"
+			sleep infinity
+		fi
+		tar -xvf $LAT_V.tar.gz
+		mv ${DATA_DIR}/MagicMirror*/* ${DATA_DIR}/
+		rm -R ${DATA_DIR}/MagicMirror*
+		rm -R ${DATA_DIR}/$LAT_V.tar.gz
+		touch ${DATA_DIR}/installed$LAT_V
+		if [ ! -f ${DATA_DIR}/modules/modules.txt ]; then
+			cp /tmp/modules.txt ${DATA_DIR}/modules/modules.txt
+		fi
+	elif [ "$LAT_V" != "$CUR_V" ]; then
+		echo "---Newer version found, installing!---"
+		mkdir -p /tmp/Backup
+		cd /tmp/Backup
+		if wget -q -nc --show-progress --progress=bar:force:noscroll -O $LAT_V.tar.gz https://github.com/MichMich/MagicMirror/archive/$LAT_V.tar.gz ; then
+			echo "---Successfully downloaded MagicMirror² $LAT_V!---"
+		else
+			echo "---Something went wrong, can't download MagicMirror² $LAT_V, putting server in sleep mode---"
+			sleep infinity
+		fi
+		mv ${DATA_DIR}/config /tmp/Backup/
+		mv ${DATA_DIR}/modules /tmp/Backup/
+		rm -R ${DATA_DIR}/*
+		rm -R ${DATA_DIR}/.*
+		mv /tmp/Backup/$LAT_V.tar.gz ${DATA_DIR}
+		cd ${DATA_DIR}
+		tar -xvf $LAT_V.tar.gz
+		mv ${DATA_DIR}/MagicMirror*/* ${DATA_DIR}/
+		rm -R ${DATA_DIR}/MagicMirror*
+		rm -R ${DATA_DIR}/$LAT_V.tar.gz
+		rm -R ${DATA_DIR}/config
+		rm -R ${DATA_DIR}/modules
+		mv /tmp/Backup/config ${DATA_DIR}/
+		mv /tmp/Backup/modules ${DATA_DIR}/
+		rm -R /tmp/Backup
+		touch ${DATA_DIR}/installed$LAT_V
+		if [ ! -f ${DATA_DIR}/modules/modules.txt ]; then
+			cp /tmp/modules.txt ${DATA_DIR}/modules/modules.txt
+		fi
+	elif [ "$LAT_V" == "$CUR_V" ]; then
+		echo "---MagicMirror² up-to-date---"
+	else
+		echo "---Something went wrong, putting server in sleep mode---"
+		sleep infinity
+	fi
 fi
+
+modules=${DATA_DIR}/modules/modules.txt
+grep -v '^\s*$\|^#\|^\s*\#' < $modules | {
+while read -r line
+do
+	if [ ! -d ${DATA_DIR}/modules/"$(echo "$(echo ${line##*/} | cut -d '.' -f1)")" ]; then
+		if [[ $line = \Y* ]]; then
+			cd ${DATA_DIR}/modules
+			git clone ${line//Y /}
+			cd ${DATA_DIR}/modules/"$(echo "$(echo ${line##*/} | cut -d '.' -f1)")"
+			npm install
+		else
+			cd ${DATA_DIR}/modules
+			git clone ${line//N /}
+		fi
+	else
+		if [ ! -d ${DATA_DIR}/.npm ]; then
+			if [[ $line = \Y* ]]; then
+				cd ${DATA_DIR}/modules/"$(echo "$(echo ${line##*/} | cut -d '.' -f1)")"
+				git pull ${line//Y /}
+				npm install
+			else
+				cd ${DATA_DIR}/modules/"$(echo "$(echo ${line##*/} | cut -d '.' -f1)")"
+				git pull ${line//N /}
+			fi
+		elif [ "${FORCE_UPDATE}" == "true" ]; then
+				if [[ $line = \Y* ]]; then
+						cd ${DATA_DIR}/modules/"$(echo "$(echo ${line##*/} | cut -d '.' -f1)")"
+						git pull ${line//Y /}
+						npm install
+				else
+						cd ${DATA_DIR}/modules/"$(echo "$(echo ${line##*/} | cut -d '.' -f1)")"
+						git pull ${line//N /}
+				fi
+		else
+			echo "Module '$(echo "$(echo ${line##*/} | cut -d '.' -f1)")' found!"
+		fi
+	fi
+done; }
 
 echo "---Preparing Server---"
 if [ ! -f ${DATA_DIR}/config/config.js ]; then
